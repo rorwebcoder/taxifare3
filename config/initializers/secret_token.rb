c05d1a0321b416a0c5b9b3aa6ad0bd3a0ead8c39
rorwebcoder <rorwebcoder@gmail.com>
2011-07-10 09:22:36 +0530

    new taxi rails 3 app ijncluded

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Taxi::Application.config.secret_token = '53b3b0a3f7015c23b6357d9c71b96f1e3c2d6c77b1343af5cdb1a0ecc2618d6a9a5f1dd1ff5a700c9b0c8b6a48a7204cba16396e3379b554640c2b4fe439131d'
